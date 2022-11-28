11f958c48cd91ee4a7f07fe45427aaed488e21c7
CodeConnoisseur74 <sherry.andrews@outlook.com>
2022-11-28 20:52:44 +0100

    add game constructor and classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,20 @@ window.addEventListener('load', function(){
 
     }
     class Player {
+        constructor(game){
+            this.game = game;
+            this.width = 120;
+            this.height = 190;
+            this.x = 20;
+            this.y = 100;
+            this.speedy = 0;
+        }
+        update(){
+            this.y += this.speedy;
+        }
+        draw(context){
+
+        }
 
     }
     class Enemy {
